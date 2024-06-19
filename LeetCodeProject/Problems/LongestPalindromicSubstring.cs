7ac9ee17b2c48dde2affebf2a86d3d0b4c9e6a4c
Adebayo Salami <salamibolarinwa16@gmail.com>
2024-06-19 23:29:29 +0100

    Push

--- LeetCodeProject/Problems/LongestPalindromicSubstring.cs
+++ LeetCodeProject/Problems/LongestPalindromicSubstring.cs
@@ -23,6 +23,9 @@ namespace LeetCodeProject.Problems
 
             try
             {
+                if (String.IsNullOrWhiteSpace(s))
+                    return ans;
+
                 long longestLength = 0;
                 var sArrays = s.ToCharArray();
                 for(int i = 0; i < sArrays.Length; i++)
@@ -64,6 +67,56 @@ namespace LeetCodeProject.Problems
             }
 
             return ans;
+        }
+
+        public static string OptLongestPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+
+            int n = s.Length;
+            bool[,] dp = new bool[n, n];
+            int start = 0;
+            int maxLen = 1;
+
+            // All substrings of length 1 are palindromes
+            for (int i = 0; i < n; i++)
+            {
+                dp[i, i] = true;
+            }
+
+            // Check for palindromes of length 2
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (s[i] == s[i + 1])
+                {
+                    dp[i, i + 1] = true;
+                    start = i;
+                    maxLen = 2;
+                }
+            }
+
+            // Check for palindromes of length >= 3
+            for (int len = 3; len <= n; len++)
+            {
+                for (int i = 0; i < n - len + 1; i++)
+                {
+                    int j = i + len - 1;
+                    if (s[i] == s[j] && dp[i + 1, j - 1])
+                    {
+                        dp[i, j] = true;
+                        if (len > maxLen)
+                        {
+                            maxLen = len;
+                            start = i;
+                        }
+                    }
+                }
+            }
+
+            return s.Substring(start, maxLen);
         }
     }
 }
