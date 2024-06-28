15b8da240e669d7261e1dadfbcbff3d06c0c0988
Adebayo-Salami <salamibolarinwa16@gmail.com>
2024-06-28 17:43:58 +0100

    Push

---
+++ LeetCodeProject/GoogleInterviewPractice/LeetArray.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProject.GoogleInterviewPractice
+{
+    public class LeetArray
+    {
+        public int _count = 0;
+        int[] _array;
+
+        public LeetArray(int size) 
+        {
+            _array = new int[size];
+        }
+
+        public void Insert(int value)
+        {
+            if (_count == _array.Length)
+                Resize();
+
+            _array[_count++] = value;
+        }
+
+        public void RemoveAt(int index) 
+        {
+            if (index >= _count || index < 0)
+                throw new IndexOutOfRangeException();
+
+            for (int i = index; i < (_count - 1); i++)
+                _array[i] = _array[i + 1];
+
+            _count--;
+        }
+
+        private void Resize()
+        {
+            if(_count == _array.Length)
+            {
+                int[] resizedArray = new int[_array.Length * 2];
+                for (int i = 0; i < _array.Length; i++)
+                    resizedArray[i] = _array[i];
+                _array = resizedArray;
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < _count; ++i)
+                Console.WriteLine(_array[i]);
+        }
+
+        public int IndexOf(int item)
+        {
+            for (int i = 0; i < _count; i++)
+                if (_array[i] == item)
+                    return i;
+
+            return -1;
+        }
+
+        public int ValueOf(int index)
+        {
+            if (index < 0 || index >= _count)
+                throw new IndexOutOfRangeException();
+
+            return _array[index];
+        }
+
+        public int Max()
+        {
+            int max = _array[0];
+
+            for (int i = 0; i < _count; i++)
+                if (_array[i] > max)
+                    max = _array[i];
+
+            return max;
+        }
+
+        public LeetArray Intersect(LeetArray array)
+        {
+            int smallestSize = _count > array._count ? array._count : _count;
+            LeetArray newArray = new LeetArray(smallestSize);
+
+            for (int i = 0; i < array._count; i++)
+                if (IndexOf(array.ValueOf(i)) != -1)
+                    newArray.Insert(array.ValueOf(i));
+
+            return newArray;
+        }
+
+        public void Reverse()
+        {
+            int[] reversedArray = new int[_count];
+            for (int i = (_count - 1), j = 0; i >= 0; i--)
+                reversedArray[j++] = _array[i];
+            _array = reversedArray;
+        }
+
+        public void InsertAt(int item, int index)
+        {
+            if (index >= _count || index < 0)
+                throw new IndexOutOfRangeException();
+
+            if ((_count + 1) > _array.Length)
+                Resize();
+
+            for (int i = _count; i >= index; i--)
+                if (i == index)
+                    _array[i] = item;
+                else
+                    _array[i] = _array[i - 1];
+            _count++;
+        }
+    }
+}
--- LeetCodeProject/Program.cs
+++ LeetCodeProject/Program.cs
@@ -1,4 +1,5 @@
-﻿using LeetCodeProject.Problems;
+﻿using LeetCodeProject.GoogleInterviewPractice;
+using LeetCodeProject.Problems;
 
 namespace LeetCodeProject
 {
@@ -17,9 +18,65 @@ namespace LeetCodeProject
             //ZigzagConversion.Run();
             //MinimumLengthSubArray.Run();
             //ReverseInteger.Run();
-            StringToInteger.Run();
+            //StringToInteger.Run();
+            RunGoogleInterviewPrepCode();
 
             Console.ReadKey();
+        }
+
+        static void RunGoogleInterviewPrepCode()
+        {
+            RunArrayClass();
+        }
+
+        static void RunArrayClass()
+        {
+            LeetArray numbers = new LeetArray(10);
+            numbers.Insert(10);
+            numbers.Insert(20);
+            numbers.Insert(30);
+            numbers.Insert(40);
+            numbers.Insert(50);
+            numbers.Insert(60);
+            numbers.Insert(70);
+            numbers.Insert(80);
+            numbers.Insert(90);
+            numbers.Insert(100);
+            numbers.RemoveAt(3);
+            Console.WriteLine(numbers.IndexOf(100));
+
+            LeetArray numbers2 = new LeetArray(10);
+            numbers2.Insert(50);
+            numbers2.Insert(60);
+            numbers2.Insert(70);
+            numbers2.Insert(80);
+            numbers2.Insert(90);
+            numbers2.Insert(100);
+            numbers2.Insert(110);
+            numbers2.Insert(120);
+            numbers2.Insert(130);
+            numbers2.Insert(140);
+
+            var intersectedNumbers = numbers.Intersect(numbers2);
+            intersectedNumbers.Print();
+
+            LeetArray numbers3 = new LeetArray(4);
+            numbers3.Insert(1);
+            numbers3.Insert(2);
+            numbers3.Insert(3);
+            numbers3.Insert(4);
+            numbers3.Reverse();
+            numbers3.Print();
+
+            Console.WriteLine("Testing Insert At");
+            LeetArray numbers4 = new LeetArray(5);
+            numbers4.Insert(1);
+            numbers4.Insert(2);
+            numbers4.Insert(3);
+            numbers4.Print();
+            Console.WriteLine("----Inserting 8 at index 1 now----");
+            numbers4.InsertAt(8, 1);
+            numbers4.Print();
         }
     }
 }
