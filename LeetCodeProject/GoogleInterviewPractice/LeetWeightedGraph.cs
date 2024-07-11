5e763d8d22bbf3a2a8a9758b6b37604129ad7614
Adebayo-Salami <salamibolarinwa16@gmail.com>
2024-07-11 22:57:41 +0100

    Push

---
+++ LeetCodeProject/GoogleInterviewPractice/LeetWeightedGraph.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProject.GoogleInterviewPractice
+{
+    public class LeetWeightedGraph
+    {
+        private class LeetWeightedGraphNode
+        {
+            private string _value;
+
+            public LeetWeightedGraphNode(string value)
+            {
+                _value = value;
+            }
+        }
+
+        private class LeetWeightedGraphEdge
+        {
+            private LeetWeightedGraphNode _from;
+            private LeetWeightedGraphNode _to;
+            private int _weight;
+
+            public LeetWeightedGraphEdge(LeetWeightedGraphNode from, LeetWeightedGraphNode to, int weight)
+            {
+                _from = from;
+                _to = to;
+                _weight = weight;
+            }
+        }
+    }
+}
