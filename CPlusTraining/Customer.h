859504e063c1037c558ba7bcaf8b1ab2b74cc05b
Adebayo-Salami <salamibolarinwa16@gmail.com>
2024-07-24 22:50:37 +0100

    Push

--- CPlusTraining/Customer.h
+++ CPlusTraining/Customer.h
@@ -13,3 +13,19 @@ struct Customer
 	bool operator == (const Customer& customer) const; //cpp reference .com operator overloading
 };
 
+ostream& operator << (ostream& stream, const Customer& customer) {
+	stream << customer.ID << endl;
+	return stream;
+}
+
+enum CustomerStatus {
+	PAYING = 1,
+	OWNING,
+	PAID
+};
+
+enum class CustomerStatus2 {
+	PAYING = 1,
+	OWNING,
+	PAID
+};
