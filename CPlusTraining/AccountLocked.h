5f5754a0ef93e83b92ba7d3755248687cf21f3c1
Adebayo-Salami <salamibolarinwa16@gmail.com>
2024-07-27 22:24:52 +0100

    Updaye

---
+++ CPlusTraining/AccountLocked.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stdexcept>
+
+using namespace std;
+
+class AccountLocked : public exception
+{
+public:
+	const char* what() const noexcept override {
+		return "Your account is locked! Contact the admin.";
+	}
+};
+
